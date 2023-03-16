c60a57d1e2d670131063ec545211730d1afdc403
Ken Bimmel <you@example.com>
2023-03-16 18:15:25 -0500

    Add basic interface and area selection

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
+import {
+  Button,
+  InputLabel,
+  NativeSelect,
+  Grid,
+  TextField,
+} from "@mui/material";
+import { LOOT_AREAS, LOOT_TABLE } from "./data/lootTables";
 
 function App() {
+  const [selectedTable, setSelectedTable] = useState(LOOT_AREAS[0]);
+  const [targetBarter, setTargetBarter] = useState(1);
+
+  const updateSelectedTable = function (event) {
+    setSelectedTable(event.target.value);
+  };
+  const updateTargetBarter = function (event) {
+    setTargetBarter(event.target.value);
+  };
+
+  const rollTable = function () {
+    const table = LOOT_TABLE[selectedTable];
+    console.log(table, targetBarter);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid item style={{ width: "100%" }}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid item>
+            <InputLabel variant="standard" htmlFor="uncontrolled-native">
+              Age
+            </InputLabel>
+            <NativeSelect
+              defaultValue={LOOT_AREAS[0]}
+              onChange={updateSelectedTable}
+              inputProps={{
+                name: "loot_area",
+                id: "uncontrolled-native",
+              }}
+            >
+              {LOOT_AREAS.map((area) => {
+                return (
+                  <option value={area} key={area}>
+                    {area}
+                  </option>
+                );
+              })}
+            </NativeSelect>
+          </Grid>
+          <Grid item>
+            <TextField
+              id="target-barter-select"
+              label="Target Barter"
+              type="number"
+              onChange={updateTargetBarter}
+            />
+          </Grid>
+          <Grid item>
+            <Button onClick={rollTable}>Roll</Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
